e358e399ab1900f25ce6ef2772e2affb25ce2ad1
Favio Manriquez Leon <favio@favrik.com>
2017-03-20 12:14:36 -0700

    [clj] Add alternative solutions for 2015-01

--- 2015/clojure-solutions/src/clojure_solutions/01.clj
+++ 2015/clojure-solutions/src/clojure_solutions/01.clj
@@ -5,8 +5,13 @@
   (->> (util/input "01")
        (map #(if (= % \() 1 -1))))
 
+(def data-alt
+  (->> (util/input "01")
+       (map {\( 1 \) -1})))
+
+
 (def final-floor
-  (->> data
+  (->> data-alt
        (reduce +)))
 
 (defn base [acc x]
@@ -21,3 +26,8 @@
        inc))
 
 
+(def at-basement-alt
+  (->> data-alt
+       (reductions + 0)
+       (take-while #(not= % -1))
+       count))
